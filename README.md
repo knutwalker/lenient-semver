822d9eb4d6a0a93958697849fbcff37c9cf705b4
Paul Horn <paul.horn@neotechnology.com>
2020-09-13 02:49:19 +0200

    Initial project structure

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # lenient-version-parser
 
+Lenient parser for Semantic Version numbers.
+
+### Motivation
+
+This crate aims to provide an alternative parser for [semver `Version`s](https://crates.io/crates/semver).
+
+Instead of adhering to the semver specification, this parser is more lenient in what it allows.
+The differenc include:
+
+- Minor and Path are optional an default to 0 (e.g. "1" parses at "1.0.0")
+- Pre-release identifier may be separated by `.` as well (e.g. "1.2.3.rc1" parses at "1.2.3-rc1")
+- Some pre-release identifiers are parsed as build identifier (e.g. "1.2.3.Final" parses at "1.2.3+Final")
 
 
 License: MIT OR Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,43 @@
+#![deny(
+    bad_style,
+    const_err,
+    dead_code,
+    improper_ctypes,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    no_mangle_generic_items,
+    non_shorthand_field_patterns,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts,
+    unconditional_recursion,
+    unsafe_code,
+    unused_allocation,
+    unused_comparisons,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_parens,
+    unused_qualifications,
+    unused_results,
+    unused,
+    while_true
+)]
 
+//! Lenient parser for Semantic Version numbers.
+//!
+//! ## Motivation
+//!
+//! This crate aims to provide an alternative parser for [semver `Version`s](https://crates.io/crates/semver).
+//!
+//! Instead of adhering to the semver specification, this parser is more lenient in what it allows.
+//! The differenc include:
+//!
+//! - Minor and Path are optional an default to 0 (e.g. "1" parses at "1.0.0")
+//! - Pre-release identifier may be separated by `.` as well (e.g. "1.2.3.rc1" parses at "1.2.3-rc1")
+//! - Some pre-release identifiers are parsed as build identifier (e.g. "1.2.3.Final" parses at "1.2.3+Final")
+//!
