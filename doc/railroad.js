188b04dbb6b9c16b1670668a42c37c2bb7c8bf3c
Paul Horn <paul.horn@neotechnology.com>
2020-09-14 21:48:16 +0200

    Update docs

--- doc/railroad.js
+++ doc/railroad.js
@@ -1,66 +1,88 @@
 // To render as railroad, paste the content of this file at
 // https://tabatkins.github.io/railroad-diagrams/generator.html
 Diagram(
-  Optional(NonTerminal('whitespace'), 'skip'),
-
-  NonTerminal('major'),
-
-  Optional(
+  Stack(
     Sequence(
-      Terminal('.'),
-      NonTerminal('minor'),
+
+      // leading whitespace is skipped
+      Optional(NonTerminal('whitespace'), 'skip'),
+
+      // require at least the major version
+      NonTerminal('major'),
+
+      // the following minor and patch versions are optional
       Optional(
         Sequence(
           Terminal('.'),
-          NonTerminal('patch')
+          NonTerminal('minor'),
+          Optional(
+            Sequence(
+              Terminal('.'),
+              NonTerminal('patch')
+            )
+          )
         )
-      )
-    )
-  ),
-
-  Choice(1,
-
-    Sequence(
-      Choice(1,
-        Terminal('-'),
-        Terminal('.'),
-        Terminal('+'),
       ),
-      Choice(0,
-        Terminal('Final'),
-        Terminal('Release')
-      )
+
     ),
 
-    Skip(),
+    // release and build identifiers
+    Choice(1,
 
-    Sequence(
-      Choice(0,
-        Terminal('-'),
-        Terminal('.')
-      ),
-      OneOrMore(
-        NonTerminal('pre-release'),
+      // any identifiers are optional
+      Skip(),
+
+      // default pre-release and build parsing
+      Sequence(
+        // pre-release delimter
         Choice(0,
           Terminal('-'),
           Terminal('.')
-        )
-      ),
-      Optional(
-        Sequence(
-          Terminal('+'),
-          OneOrMore(
-            NonTerminal('build'),
-            Choice(0,
-              Terminal('-'),
-              Terminal('.')
-            )
+        ),
+        // followed by a pre-release identifier
+        // or many of them delimited with the same delimiters
+        OneOrMore(
+          NonTerminal('pre-release'),
+          Choice(0,
+            Terminal('-'),
+            Terminal('.')
           )
         ),
-        'skip'
-      )
-    )
-  ),
 
-  Optional(NonTerminal('whitespace'), 'skip'),
+        // last optional but, the build identifiers
+        Choice(0,
+          Sequence(
+            // build delimter
+            Terminal('+'),
+            // followed by a build identifier
+            // or many of them delimited with the same delimiters as the pre-release identifiers
+            OneOrMore(
+              NonTerminal('build'),
+              Choice(0,
+                Terminal('-'),
+                Terminal('.')
+              )
+            )
+          ),
+          Skip(),
+        )
+      ),
+
+      // special branch into `.Final` or `.Release` build identifiers
+      Sequence(
+        Choice(1,
+          Terminal('-'),
+          Terminal('.'),
+          Terminal('+'),
+        ),
+        Choice(0,
+          Terminal('Final'),
+          Terminal('Release')
+        )
+      ),
+    ),
+
+    // trailing whitespace is skipped
+    Optional(NonTerminal('whitespace'), 'skip'),
+  ),
 )
